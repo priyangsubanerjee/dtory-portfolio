c0deb0a97e424f2a600b39a2c3ee106b7d5919df
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-12 19:56:40 +0530

    anim

--- components/NavDtory.js
+++ components/NavDtory.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function NavDtory() {
+  const [height, setHeight] = useState(0);
+  useEffect(() => {
+    // increase height to 446px, 1px per 10ms
+
+    let interval = setInterval(() => {
+      setHeight((prev) => {
+        if (prev < 446) {
+          return prev + 1;
+        }
+        clearInterval(interval);
+        return prev;
+      });
+    }, 0.5);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div className="flex justify-center pt-16 px-[100px] h-full">
-      <div className="absolute top-0 left-[24%] h-[446px] w-[1px] bg-[#978DFF]"></div>
-      <div className="absolute top-0 left-[44%] h-[446px] w-[1px] bg-[#978DFF]"></div>
-      <div className="absolute top-0 left-[80%] h-[446px] w-[1px] bg-[#978DFF]"></div>
+      <div
+        style={{
+          height: `${height}px`,
+        }}
+        className="absolute top-0 left-[24%] w-[1px] bg-[#978DFF]"
+      ></div>
+      <div className="absolute top-0 left-[44%] w-[1px] bg-[#978DFF]"></div>
+      <div className="absolute top-0 left-[80%] w-[1px] bg-[#978DFF]"></div>
       <ul className="space-y-9 text-3xl font-thin">
         <li>About us</li>
         <li>Career</li>
